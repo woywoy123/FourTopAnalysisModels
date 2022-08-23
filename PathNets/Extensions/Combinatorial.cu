8a30720985be571b9c323f8b308ba4d7c9aa12c8
woywoy123 <None>
2022-08-24 04:06:31 +1000

    Need to figure out the summation part.

--- PathNets/Extensions/Combinatorial.cu
+++ PathNets/Extensions/Combinatorial.cu
@@ -1,27 +1,60 @@
 #include <torch/extension.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <iostream>
 
-
+template <typename scalar_t>
+__device__ __forceinline__ scalar_t VecCalc(scalar_t fv, scalar_t sw)
+{
+	return fv*sw; 
+}
 
 
 template <typename scalar_t>
-__global__ void Kernel(
-		const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> PTH, 
-		const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> FV, 
+__global__ void MassKernel(
+	scalar_t* __restrict__ Mass,
+	const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> PTH, 
+	const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> FV, 
+	torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> Pmu,
+	const size_t cmbi_l, 
+	const size_t nodes)
+{
+	const int nd = blockIdx.y;
+	const int index = blockIdx.x*blockDim.x + threadIdx.x;
+	if (index < nodes)
+	{
+		printf("%d, %d\n", nd, index);
+		Pmu[nd][0] += FV[index][0];
+		Pmu[nd][1] += FV[index][1];
+		Pmu[nd][2] += FV[index][2];
+		Pmu[nd][3] += FV[index][3];
+	}
 
-
-
-
+}
 torch::Tensor PathMassGPU(torch::Tensor AdjMatrix, torch::Tensor FourVector)
 {
-	const int l = AdjMatrix.size(0);
+	torch::TensorOptions options = torch::TensorOptions().dtype(torch::kFloat).device(torch::kCUDA);
+	const int cmbi_l = AdjMatrix.size(0);
+	const int nodes = AdjMatrix.size(1);
+	std::cout << nodes << " " << cmbi_l << std::endl;
+	const int threads = nodes;
+	const dim3 blocks((nodes + threads -1) / threads, cmbi_l);
 	
+	torch::Tensor Mass = torch::zeros({cmbi_l, 1}, options);
+	torch::Tensor Pmu = torch::zeros({cmbi_l, 4}, options);
+	FourVector = FourVector.to(options);
+	AdjMatrix = AdjMatrix.to(options);
 
-
-
-
-
-	return AdjMatrix;
+	AT_DISPATCH_FLOATING_TYPES(torch::kFloat, "MassKernel", ([&]
+	{
+		MassKernel<scalar_t><<<blocks, threads>>>(
+			Mass.data_ptr<scalar_t>(),
+			AdjMatrix.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(), 
+			FourVector.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+			Pmu.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+			cmbi_l, nodes
+		);
+	}));
+	return Pmu;
 
 }
--- PathNets/Extensions/Test.py
+++ PathNets/Extensions/Test.py
@@ -2,19 +2,23 @@ import torch
 from PathNetOptimizer import *
 from PathNetOptimizerCUDA import * 
 
-AdjM = PathCombinatorial(3, 3, "cpu")
-TestVect = torch.Tensor([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], device = "cpu")
+#AdjM = PathCombinatorial(3, 3, "cpu")
+#TestVect = torch.Tensor([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], device = "cpu")
+#print(AdjM)
+#print(TestVect)
+#
+#for i in AdjM:
+#    print("-> ", i)
+#    
+#    print("-----")
+#    print(TestVect[i == 1])
+#
+#print(PathMassCPU(AdjM, TestVect))
+
+AdjM = PathCombinatorial(3, 3, "cuda")
+TestVect = torch.tensor([[8, 8, 1, 1], [1, 2, 2, 1], [4, 4, 3, 3]], device = "cuda")
 print(AdjM)
 print(TestVect)
 
-for i in AdjM:
-    print("-> ", i)
-    
-    print("-----")
-    print(TestVect[i == 1])
-
-print(PathMassCPU(AdjM, TestVect))
-
-AdjM = PathCombinatorial(3, 3, "cuda")
-TestVect = torch.tensor([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], device = "cuda")
-PathMassCUDA(AdjM, TestVect)
+print("-----")
+print(PathMassCUDA(AdjM, TestVect))
