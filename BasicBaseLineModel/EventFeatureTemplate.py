62404ddb07a4e79cb31af3f717696d6fc851b528
woywoy123 <thomasmomensen@gmail.com>
2022-09-18 20:44:20 +1000

    Starting new BaseLine.

--- BasicBaseLineModel/EventFeatureTemplate.py
+++ BasicBaseLineModel/EventFeatureTemplate.py
@@ -2,14 +2,13 @@ import Templates.ParticleGeneric.EdgeFeature as ef
 import Templates.ParticleGeneric.NodeFeature as nf
 import Templates.ParticleGeneric.GraphFeature as gf
 
-import Templates.TruthTopChildren.NodeFeature as tc_nf
-
-import Templates.TruthJet.EdgeFeature as tj_ef
-import Templates.TruthJet.NodeFeature as tj_nf
-import Templates.TruthJet.GraphFeature as tj_gf
 from AnalysisTopGNN.Tools.ModelTesting import AddFeature
 
 def TruthJets():
+    import Templates.TruthJet.EdgeFeature as tj_ef
+    import Templates.TruthJet.NodeFeature as tj_nf
+    import Templates.TruthJet.GraphFeature as tj_gf
+
     # Node: Generic Particle Properties
     GenPartNF = {
             "eta" : nf.eta, 
@@ -63,8 +62,60 @@ def TruthJets():
     Features |= AddFeature("GT", GenPartTGF)
     return Features
 
-def ApplyFeatures(A):
-    Features = TruthJets()
+def TruthTopChildren():
+    import Templates.TruthTopChildren.NodeFeature as tc_nf
+    
+    # Node: Generic Particle Properties
+    GenPartNF = {
+            "eta" : nf.eta, 
+            "energy" : nf.energy, 
+            "pT" : nf.pT, 
+            "phi" : nf.phi, 
+            "mass" : nf.mass, 
+            "islep" : nf.islepton, 
+            "charge" : nf.charge, 
+    }
+    
+    # Graph: Generic Particle Properties
+    GenPartGF = {
+            "mu" : gf.mu, 
+            "met" : gf.met, 
+            "met_phi" : gf.met_phi, 
+    }
+    
+    # Truth Edge: Truth Children Properties
+    ChildrenTEF = {
+            "edge" : ef.Index, 
+    } 
+    
+    # Truth Node: Truth Children Properties
+    ChildrenTNF = {
+            "from_res" : tc_nf.FromRes, 
+    } 
+    
+    # Truth Graph: Generic Paritcle Properties
+    GenPartTGF = {
+            "nTops" : gf.nTops, 
+            "signal_sample" : gf.SignalSample
+    }
+    
+    Features = {}
+    Features |= AddFeature("NF", GenPartNF)
+    Features |= AddFeature("GF", GenPartGF)
+    Features |= AddFeature("ET", ChildrenTEF)
+    Features |= AddFeature("NT", ChildrenTNF)    
+    Features |= AddFeature("GT", GenPartTGF)
+
+    return Features
+ 
+
+def ApplyFeatures(A, Level):
+    if Level == "TruthChildren":
+        Features = TruthJets()
+
+    if Level == "TruthJets":
+        Features = TruthJets()
+
     for i in Features:
         base = "_".join(i.split("_")[1:])
         fx = Features[i]
--- BasicBaseLineModel/RunBasicBaseLine.py
+++ BasicBaseLineModel/RunBasicBaseLine.py
@@ -1,8 +1,8 @@
 from AnalysisTopGNN.Tools.ModelTesting import CreateWorkspace, KillCondition, OptimizerTemplate
 from AnalysisTopGNN.Generators import Analysis
-from AnalysisTopGNN.Events import Event, EventGraphTruthJetLepton
+from AnalysisTopGNN.Events import Event
 from BasicBaseLine import *
-from EventFeatureTemplate import TruthJets
+from EventFeatureTemplate import TruthJets, TruthTopChildren
 
 def BaseLineModelTruthJet(Files, Names, CreateCache):
     CreateCache = False
@@ -30,69 +30,30 @@ def BaseLineModelTruthJet(Files, Names, CreateCache):
     #kill |= {"from_top": "C"}
     KillCondition(kill, 50, Op, samples[k], 100000, sleep = 2, batched = 3)
 
+def BasicBaseLineTruthChildren(Files, Names, CreateCache):
+    Features = TruthTopChildren()
+    DL = CreateWorkspace(Files, Features, CreateCache, 100, Names, "TruthTopChildren", True)
+    samples = DL.TrainingSample
+ 
+    Model = BasicBaseLine()
+    Op = OptimizerTemplate(DL, Model)
+    Op.LearningRate = 0.0001
+    Op.WeightDecay = 0.001
+    Op.DefineOptimizer()
 
-def BaseLineModelTruthJetAnalysis():
-    GeneralDir = "/CERN/CustomAnalysisTopOutputTest/"
-    TopBackgrounds = [GeneralDir + "t", GeneralDir + "ttbar"]
-    Signal = [GeneralDir + "tttt"]
-    
-    A = Analysis()
-    
-    # Config Settings
-    A.ProjectName = "TopEvaluation"
-    A.EventImplementation = Event
-    A.CompileSingleThread = False
-    A.CPUThreads = 4
-    A.EventCache = False
-    A.DataCache = False
-    A.GenerateTrainingSample = False
-    A.TrainWithoutCache = True
+    kill = {}
+    kill |= {"edge" : "R"}
+    #kill |= {"from_res" : "C"}
+    #kill |= {"signal_sample": "C"}
+    #kill |= {"from_top": "C"}
+    KillCondition(kill, 1000, Op, samples, 100000, sleep = 2, batched = 3)
 
-    A.EventGraph = EventGraphTruthJetLepton
-    A.TrainingSampleSize = 80
-    A.Device = "cuda"
-    A.SelfLoop = True
-    A.FullyConnect = True
-    #A.NEvent_Stop = 100
-
-    A.Model = BasicBaseLineTruthJet()
-    A.LearningRate = 0.0001
-    A.WeightDecay = 0.0001
-    A.kFold = 2
-    A.Epochs = 3
-    A.BatchSize = 20
-    A.RunName = "BasicBaseLineTruthJet"
-    A.ONNX_Export = True
-    A.TorchScript_Export = True
-
-    Features = TruthJets()
-    for i in Features:
-        base = "_".join(i.split("_")[1:])
-        fx = Features[i]
-        
-        if "EF" in i:
-            A.AddEdgeFeature(base, fx)
-        elif "NF" in i:
-            A.AddNodeFeature(base, fx)
-        elif "GF" in i:
-            A.AddGraphFeature(base, fx)
-
-        elif "ET" in i:
-            A.AddEdgeTruth(base, fx)
-        elif "NT" in i:
-            A.AddNodeTruth(base, fx)
-        elif "GT" in i:
-            A.AddGraphTruth(base, fx)
-
-
-    A.InputSample("TopBackground", TopBackgrounds)
-    A.InputSample("SignalSample", Signal)
-    A.Launch()
 
 if __name__ == "__main__":
     GeneralDir = "/CERN/CustomAnalysisTopOutputTest/"
     Files = [GeneralDir + "tttt/QU_0.root", GeneralDir + "t/QU_0.root"]
-    Names = ["tttt", "t"]
-    CreateCache = True
-    BaseLineModelTruthJet(Files, Names, CreateCache)
-    #BaseLineModelTruthJetAnalysis()
+    Names = ["tttt"]
+    CreateCache = False
+    #BaseLineModelTruthJet(Files, Names, CreateCache)
+    BasicBaseLineTruthChildren(Files, Names, CreateCache)
+
